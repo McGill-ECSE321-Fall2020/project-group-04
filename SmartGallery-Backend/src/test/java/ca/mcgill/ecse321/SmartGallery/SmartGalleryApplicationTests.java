ea577283d30067f0dcc72e7967d7955a57cefd34
Viet-T07 <viet.tran.gm@gmail.com>
2020-10-05 19:39:51 -0400

    initializing backend

---
+++ SmartGallery-Backend/src/test/java/ca/mcgill/ecse321/SmartGallery/SmartGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.SmartGallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
