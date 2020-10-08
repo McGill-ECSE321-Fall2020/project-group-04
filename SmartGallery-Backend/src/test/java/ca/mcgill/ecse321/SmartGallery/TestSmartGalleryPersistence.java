4c022ee0e2b2e22f304ce81e67b1926c57628e67
Zach <zachary.siciliani@mail.mcgill.ca>
2020-10-08 15:44:13 -0400

    Test passes locally

---
+++ SmartGallery-Backend/src/test/java/ca/mcgill/ecse321/SmartGallery/TestSmartGalleryPersistence.java
@@ -0,0 +1,35 @@
+package ca.mcgill.ecse321.SmartGallery;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import ca.mcgill.ecse321.smartgallery.dao.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class TestSmartGalleryPersistence {
+	/*
+	@Autowired
+	private ArtistRepository artistRepository;
+	@Autowired
+	private ArtworkRepository artworkRepository;
+
+	@AfterEach
+	public void clearDatabase() {
+		artworkRepository.deleteAll();
+	*/
+}
+	
