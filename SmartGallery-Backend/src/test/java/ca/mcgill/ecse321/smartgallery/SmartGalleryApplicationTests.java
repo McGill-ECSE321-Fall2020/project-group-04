9a356dcce11218d98f05b48c331031f5e28ed606
Viet-t07 <viet.tran.gm@gmail.com>
2020-10-09 10:25:56 -0400

    refactor

--- SmartGallery-Backend/src/test/java/ca/mcgill/ecse321/smartgallery/SmartGalleryApplicationTests.java
+++ SmartGallery-Backend/src/test/java/ca/mcgill/ecse321/smartgallery/SmartGalleryApplicationTests.java
@@ -1,4 +1,4 @@
-package ca.mcgill.ecse321.SmartGallery;
+package ca.mcgill.ecse321.smartgallery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
--- SmartGallery-Backend/src/test/java/ca/mcgill/ecse321/smartgallery/TestSmartGalleryPersistence.java
+++ SmartGallery-Backend/src/test/java/ca/mcgill/ecse321/smartgallery/TestSmartGalleryPersistence.java
@@ -1,4 +1,4 @@
-package ca.mcgill.ecse321.SmartGallery;
+package ca.mcgill.ecse321.smartgallery;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
