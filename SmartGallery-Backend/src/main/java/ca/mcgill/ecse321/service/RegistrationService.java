ac336edc5f7052526092e2d3ce8b2e7bf9b26f19
roeywine <roey.wine@mail.mcgill.ca>
2020-10-24 21:31:11 -0600

    Wrote some service methods for Artists and some small changes to customer service methods

--- SmartGallery-Backend/src/main/java/ca/mcgill/ecse321/service/RegistrationService.java
+++ SmartGallery-Backend/src/main/java/ca/mcgill/ecse321/service/RegistrationService.java
@@ -21,7 +21,7 @@ public class RegistrationService {
 	
 	/**
 	 * @author roeywine
-	 * Method that creates and saves customer
+	 * Method that creates and saves a customer
 	 * 
 	 * @param username 				Username for the profile
 	 * @param password 				Password for the profile
@@ -31,6 +31,7 @@ public class RegistrationService {
 	 * @param smartGallery 			The smartGallery system
 	 * @param transactions			The transactions the customer has/had
 	 * @param artworksViewed		Artwork pieces viewed by the customer
+	 * @return customer				The created customer
 	 */
 	@Transactional
 	public Customer createCustomer(String username, String password, String email, PaymentMethod defaultPaymentMethod,
@@ -48,7 +49,6 @@ public class RegistrationService {
 		customer.setArtworksViewed(artworksViewed);
 		customerRepository.save(customer);		// Save to customer repository
 		return customer;						// Return customer with updated parameters
-
 	}
 	
 	/**
@@ -70,8 +70,68 @@ public class RegistrationService {
 	 * Method that gets all existing customers
 	 */
 	@Transactional
-	public List<Customer> getAllCustomer() {
+	public List<Customer> getAllCustomers() {
+		
+		// Uses existing method in customer repository to find all the existing customers
 		return toList(customerRepository.findAll());
+	}
+	
+	/**
+	 * @author roeywine
+	 * Method that creates and saves an artist
+	 * 
+	 * @param username 				Username for the profile
+	 * @param password 				Password for the profile
+	 * @param email 				Email to be used in the profile
+	 * @param defaultPaymentMethod 	A default payment method for purchases
+	 * @param creationDate 			Date the customer profile was created
+	 * @param isVerified			Whether or not the author is verified by the system
+	 * @param smartGallery 			The smartGallery system
+	 * @param transactions			The transactions the customer has/had
+	 * @param artworks				Artwork pieces created by the artist
+	 */
+	@Transactional
+	public Artist createArtist(String username, String password, String email, PaymentMethod defaultPaymentMethod,
+			Date creationDate, boolean isVerified, SmartGallery smartGallery, Set<Transaction> transactions, Set<Artwork> artworks) {
+		
+		// Create a customer object and initialize all parameters
+		Artist artist = new Artist();
+		artist.setUsername(username);
+		artist.setPassword(password);
+		artist.setEmail(email);
+		artist.setDefaultPaymentMethod(defaultPaymentMethod);
+		artist.setCreationDate(creationDate);
+		artist.setIsVerified(false); 			// When an artists profile is made, they are not verified initially
+		artist.setSmartGallery(smartGallery);
+		artist.setTransaction(transactions);
+		artist.setArtworks(artworks);
+		artistRepository.save(artist);			// Save to customer repository
+		return artist;							// Return artist with updated parameters
+	}
+	
+	/**
+	 * @author roeywine
+	 * Method that gets an existing artist given a username
+	 * 
+	 * @param username 				Username for the profile
+	 */
+	@Transactional
+	public Artist getArtist(String username) {
+		
+		// Uses existing method in artist repository to find an artist by username
+		Artist artist = artistRepository.findArtistByUsername(username);
+		return artist;
+	}
+	
+	/**
+	 * @author roeywine
+	 * Method that gets all existing artists
+	 */
+	@Transactional
+	public List<Artist> getAllArtists() {
+		
+		// Uses existing method in customer repository to find all the existing customers
+		return toList(artistRepository.findAll());
 	}
 	
 	// Helper method to retrieve lists of objects
