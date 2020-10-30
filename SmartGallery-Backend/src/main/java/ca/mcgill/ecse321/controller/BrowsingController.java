de3e179ceb0c8298b4a071c0fb0793c49607a6fc
Zach <zachary.siciliani@mail.mcgill.ca>
2020-10-29 20:29:56 -0400

    Minor controller work

--- SmartGallery-Backend/src/main/java/ca/mcgill/ecse321/controller/BrowsingController.java
+++ SmartGallery-Backend/src/main/java/ca/mcgill/ecse321/controller/BrowsingController.java
@@ -11,7 +11,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.mcgill.ecse321.service.BrowsingService;
+import ca.mcgill.ecse321.smartgallery.dao.ArtistRepository;
+import ca.mcgill.ecse321.smartgallery.dao.ArtworkRepository;
+import ca.mcgill.ecse321.smartgallery.dao.CustomerRepository;
+import ca.mcgill.ecse321.smartgallery.dao.GalleryRepository;
+import ca.mcgill.ecse321.smartgallery.dao.ListingRepository;
+import ca.mcgill.ecse321.smartgallery.dao.SmartGalleryRepository;
+import ca.mcgill.ecse321.smartgallery.dao.TransactionRepository;
+import ca.mcgill.ecse321.smartgallery.dto.GalleryDTO;
 import ca.mcgill.ecse321.smartgallery.dto.SmartGalleryDTO;
+import ca.mcgill.ecse321.smartgallery.model.Gallery;
 import ca.mcgill.ecse321.smartgallery.model.SmartGallery;
 
 @CrossOrigin(origins = "*")
@@ -19,10 +28,25 @@ import ca.mcgill.ecse321.smartgallery.model.SmartGallery;
 public class BrowsingController {
 
 	@Autowired
+	private static ArtistRepository artistRepository;
+	@Autowired
+	private static ArtworkRepository artworkRepository;
+	@Autowired
+	private static CustomerRepository customerRepository;
+	@Autowired
+	private static GalleryRepository galleryRepository;
+	@Autowired
+	private static ListingRepository listingRepository;
+	@Autowired
+	private static SmartGalleryRepository smartGalleryRepository;
+	@Autowired
+	private static TransactionRepository transactionRepository;
+	
+	@Autowired
 	private BrowsingService browsingService;
 
 	@PostMapping(value = { "/smartGallery/{smartGalleryID}", "/smartGallery/{smartGalleryID}/" })
-	public SmartGalleryDTO createEvent(@PathVariable("smartGalleryID") int smartGalleryID)
+	public SmartGalleryDTO createSmartGallery(@PathVariable("smartGalleryID") int smartGalleryID)
 			throws IllegalArgumentException {
 		SmartGallery smartGallery = browsingService.createSmartGallery(smartGalleryID);
 		return Converters.convertToDto(smartGallery);
@@ -35,6 +59,25 @@ public class BrowsingController {
 			smartGalleryDTOs.add(Converters.convertToDto(smartGallery));
 		}
 		return smartGalleryDTOs;
+	}
+	
+	@PostMapping(value = {"/gallery/{smartGalleryID}/{galleryName}/{commission}",
+			"/gallery/{smartGalleryID}/{galleryName}/{commission}/" })
+	public GalleryDTO createGallery(@PathVariable("smartGalleryID") int smartGalleryID,
+			@PathVariable("galleryName") String galleryName, @PathVariable("commission") int commission)
+			throws IllegalArgumentException {
+		SmartGallery smartGallery = smartGalleryRepository.findSmartGalleryBySmartGalleryID(smartGalleryID);
+		Gallery gallery = browsingService.createGallery(galleryName, smartGallery, commission);
+		return Converters.convertToDto(gallery);
+	}
+	
+	@GetMapping(value = { "/gallery", "/gallery/"})
+	public List<GalleryDTO> getAllGalleries() {
+		List<GalleryDTO> galleryDTOs = new ArrayList<>();
+		for(Gallery gallery : browsingService.getAllGalleries()) {
+			galleryDTOs.add(Converters.convertToDto(gallery));
+		}
+		return galleryDTOs;
 	}
 
 }
