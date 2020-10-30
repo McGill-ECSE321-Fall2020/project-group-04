cc9759f9d1cb855aaa2043379f076d4dcf56e015
Oliver Stappas <o.stappas@gmail.com>
2020-10-30 18:56:12 -0400

    Set up unit testing with Mockito

--- SmartGallery-Backend/src/test/java/ca/mcgill/ecse321/smartgallery/BrowsingServiceTests.java
+++ SmartGallery-Backend/src/test/java/ca/mcgill/ecse321/smartgallery/BrowsingServiceTests.java
@@ -1,5 +1,10 @@
 package ca.mcgill.ecse321.smartgallery;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
 public class BrowsingServiceTests {
 
 }
