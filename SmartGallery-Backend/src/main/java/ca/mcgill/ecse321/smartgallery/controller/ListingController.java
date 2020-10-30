2a319545a319d3b7f07c81f4b0e4f6f25f1c978d
stavmits <stavrosmitsoglou@gmail.com>
2020-10-29 20:34:12 -0400

    autowired annotations added

--- SmartGallery-Backend/src/main/java/ca/mcgill/ecse321/smartgallery/controller/ListingController.java
+++ SmartGallery-Backend/src/main/java/ca/mcgill/ecse321/smartgallery/controller/ListingController.java
@@ -26,7 +26,9 @@ public class ListingController {
 
 	@Autowired
 	private ListingService listingService;
+	@Autowired
 	private ArtworkRepository artworkRepository;
+	@Autowired
 	private GalleryRepository galleryRepository;
 	
 
