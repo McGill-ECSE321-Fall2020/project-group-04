c35e72cbe48c90fda6b6592a25414311ced56701
Oliver Stappas <o.stappas@gmail.com>
2020-11-09 18:29:07 -0500

    Just started artworkSearch

---
+++ SmartGallery-Frontend/src/components/artworkSearch.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+var config = require('../../config')
+
+var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+
+var AXIOS = axios.create({
+  baseURL: backendUrl,
+  headers: { 'Access-Control-Allow-Origin': frontendUrl }
+})
+
+function ListingDto(gallery, artwork, listedDate, isSold, listingID) {
+	this.gallery = gallery;
+	this.artwork = artwork;
+	this.listedDate = listedDate;
+	this.isSold = isSold;
+	this.listingID = listingID;
+}
+
+function ArtworkDto(artists, gallery, name, year, price, isBeingPromoted, style, height, weight, width, artworkID) {
+	this.artists = artists;
+	this.gallery = gallery;
+	this.name = name;
+	this.year = year;
+	this.price = price;
+	this.isBeingPromoted = isBeingPromoted;
+	this.artStyle = style;
+	this.height = height;
+	this.weight = weight;
+	this.width = width;
+	this.artworkID = artworkID;
+}
+
+
+
--- SmartGallery-Frontend/src/router/index.js
+++ SmartGallery-Frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import ArtworkSearch from '@/components/ArtworkSearch'
+
 
 Vue.use(Router)
 
@@ -10,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/artworkSearch',
+      name: 'ArtworkSearch',
+      component: ArtworkSearch
     }
   ]
 })
