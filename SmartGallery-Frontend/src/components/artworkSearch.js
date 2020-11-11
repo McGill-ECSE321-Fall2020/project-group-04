58b45c8e59ebcb6fccf8433bba0373363d065214
Oliver Stappas <o.stappas@gmail.com>
2020-11-11 14:43:27 -0500

    Still lost

--- SmartGallery-Frontend/src/components/artworkSearch.js
+++ SmartGallery-Frontend/src/components/artworkSearch.js
@@ -5,8 +5,8 @@ var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
 var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
 
 var AXIOS = axios.create({
-  baseURL: backendUrl,
-  headers: { 'Access-Control-Allow-Origin': frontendUrl }
+	baseURL: backendUrl,
+	headers: { 'Access-Control-Allow-Origin': frontendUrl }
 })
 
 function ListingDto(gallery, artwork, listedDate, isSold, listingID) {
@@ -31,5 +31,44 @@ function ArtworkDto(artists, gallery, name, year, price, isBeingPromoted, style,
 	this.artworkID = artworkID;
 }
 
+export default {
+	name: 'artworksearch',
+	data() {
+		return {
+			listings: [],
+			errorListing: '',
+			response: []
+		}
+	},
+	created: function () {
+		AXIOS.get('/listing/artworkSearch/{searchInput}/{minPrice}/{maxPrice}/{style}')
+			.then(response => {
+				this.listings = response.data
+				console.log(response)
+			})
+			.catch(e => {
+				this.errorListing = e
+			})
+	},
+	methods: {
+		getListing: function (listingID) {
+			AXIOS.get('/listing/'.concat(listingID))
+				.then(response => {
+					this.listings = [response.data]
+					this.artwork = this.listings.artwork
+				})
+				.catch(e => {
+					this.errorListing = e
+				})
+		}
 
+	}
+}
+"/listing/artworkSearch/{searchInput}/{minPrice}/{maxPrice}/{style}", "/listing/artworkSearch/{searchInput}/{minPrice}/{maxPrice}/{style}/" 
+
+"/listing/artworkSearch/{searchInput}/{minPrice}/{maxPrice}", "/listing/artworkSearch/{searchInput}/{minPrice}/{maxPrice}/" 
+
+"/listing/artworkSearch/{searchInput}/{style}", "/listing/artworkSearch/{searchInput}/{style}/"
+
+"/listing/artworkSearch/{searchInput}", "/listing/artworkSearch/{searchInput}/" 
 
