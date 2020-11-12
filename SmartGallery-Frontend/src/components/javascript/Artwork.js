81f446c90f7163433264eaabd78fc2b596455fe9
Viet Tran <viet.tran.gm@gmail.com>
2020-11-11 22:21:47 -0500

    fixed merge issues

--- SmartGallery-Frontend/src/components/javascript/Artwork.js
+++ SmartGallery-Frontend/src/components/javascript/Artwork.js
@@ -5,35 +5,37 @@ var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
 var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
 
 var AXIOS = axios.create({
-	baseURL: backendUrl,
-	headers: { 'Access-Control-Allow-Origin': frontendUrl }
+  baseURL: backendUrl,
+  headers: {
+    'Access-Control-Allow-Origin': frontendUrl
+  }
 })
 
-  function ArtworkDTO(artists, gallery, name, year, price, isBeingPromoted, style, height, weight, width, artworkID) {
-		this.artists = artists;
-		this.gallery = gallery;
-		this.name = name;
-		this.year = year;
-		this.price = price;
-		this.isBeingPromoted = isBeingPromoted;
-		this.artStyle = style;
-		this.height = height;
-		this.weight = weight;
-		this.width = width;
-		this.artworkID = artworkID;
-	}
+function ArtworkDTO(artists, gallery, name, year, price, isBeingPromoted, style, height, weight, width, artworkID) {
+  this.artists = artists;
+  this.gallery = gallery;
+  this.name = name;
+  this.year = year;
+  this.price = price;
+  this.isBeingPromoted = isBeingPromoted;
+  this.artStyle = style;
+  this.height = height;
+  this.weight = weight;
+  this.width = width;
+  this.artworkID = artworkID;
+}
 
- export default {
-  data () {
+export default {
+  data() {
     return {
       artworks: [],
       artwork: '',
       errorArtwork: '',
-  
+
       response: [],
-      }
+    }
   },
-  created: function (){
+  created: function() {
     AXIOS.get('/artwork')
       .then(response => {
         this.artworks = response.data
@@ -44,75 +46,25 @@ var AXIOS = axios.create({
       })
   },
   methods: {
-    getArtwork: function (artworkID) {
+    getArtwork: function(artworkID) {
       AXIOS.get('/artwork/'.concat(artworkID))
-      .then(response => {
-        this.artworks = [response.data]
-        this.artwork = this.artworks.artwork
-      })
-      .catch(e => {
-        this.errorArtwork = e
-      })
+        .then(response => {
+          this.artworks = [response.data]
+          this.artwork = this.artworks.artwork
+        })
+        .catch(e => {
+          this.errorArtwork = e
+        })
     },
-    createArtwork: function (artworkName, year, price, style, height, weight, width, artist, gallery){
-      AXIOS.post('/artist/' .concat(artworkName) + '?year='+ year + '&price=' + price 
-      +'&style='+style + '&height=' + height  + '&weight=' + weight  + '&width=' + width +  + '&artist=' + artist  + '&gallery=' + gallery)
-      .then(response => {
-        this.artwork= response.data
-      })
-      .catch(e => {
-        this.errorArtwork = e
-      })
+    createArtwork: function(artworkName, year, price, style, height, weight, width, artist, gallery) {
+      AXIOS.post('/artist/'.concat(artworkName) + '?year=' + year + '&price=' + price +
+          '&style=' + style + '&height=' + height + '&weight=' + weight + '&width=' + width + +'&artist=' + artist + '&gallery=' + gallery)
+        .then(response => {
+          this.artwork = response.data
+        })
+        .catch(e => {
+          this.errorArtwork = e
+        })
     }
   }
 }
-  export default {
-  name: 'listinginfo',
-  props: ['listingId'],
-  data () {
-    return {
-     artwork: [],
-	  errorArtwork: '',
-      newArtwork: '',
-      errorNewArtwork: '',
-     
-      response: [],
-   
-      }
-  },
-  created: function (){
-    AXIOS.get('/artwork')
-      .then(response => {
-        this.artwork = response.data
-        console.log(response)
-      })
-      .catch(e => {
-        this.errorArtwork = e
-      })
-  },
-  methods: {
-    getArtwork: function (artworkID) {
-      AXIOS.get('/artwork/'.concat(artworkID))
-      .then(response => {
-        this.artwork = [response.data]
-        this.artwork = this.listings.artwork
-      })
-      .catch(e => {
-        this.errorListing = e
-      })
-    },
-    createTransaction: function (paymentMethod, deliveryMethod, username, listingID){
-      AXIOS.post('/transaction/?paymentMethod='+ paymentMethod + '&deliveryMethod='
-      +deliveryMethod+'&username='+username + '&listingID=' + listingID)
-      .then(response => {
-        this.transaction = response.data
-      })
-      .catch(e => {
-        this.errorTransaction = e
-      })
-    }
-  }
-}
-
-
-
